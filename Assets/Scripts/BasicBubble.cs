760208ca84ebedadc506e0009b600b3cf9a8d589
superpi2 <superpi2.Aa.Xx@gmail.com>
2025-01-25 16:21:13 -0500

    Bubble freeze effect, cloud platforms

--- Assets/Scripts/BasicBubble.cs
+++ Assets/Scripts/BasicBubble.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class BasicBubble : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    float freezeAmount;
+    int freezeZones;
+
+    public SpriteRenderer freezeOverlay;
+
+    private void Start()
     {
-        if (collision.gameObject.tag == "Sharp")
-            Pop();
+        freezeAmount = 0f;
+        freezeZones = 0;
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void Update()
     {
-        if (collision.gameObject.tag == "Freeze")
+        if (freezeZones == 0)
+            freezeAmount -= Time.deltaTime;
+        else
+            freezeAmount += Time.deltaTime;
+
+        freezeAmount = Mathf.Clamp01(freezeAmount);
+
+        if (freezeAmount >= 1f)
             GameManager.instance.TransformBubble(gameObject, GameManager.instance.frozenBubble);
+
+        freezeOverlay.color = new Color(1f, 1f, 1f, Mathf.Pow(freezeAmount, 2f));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject);
+
+        if (collision.gameObject.tag == "Sharp")
+            Pop();
+
+        if (collision.gameObject.tag == "Freeze")
+            freezeZones += 1;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject);
+
+        if (collision.gameObject.tag == "Freeze")
+            freezeZones -= 1;
     }
 
     protected void Pop()
